156ead4c9a5867ebc13e975cb213a7fbe6dfbe46
janfasola <odense123101@yahoo.dk>
2014-03-20 22:47:40 +0100

    some shit
    done smth

--- tcpclient/Program.cs
+++ tcpclient/Program.cs
@@ -2,13 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Net.Sockets;
+using System.IO;
 namespace tcpclient
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
+            TcpClient client = new TcpClient("62.75.224.28", 6789);
+            int a = 0;
+            try
+            {
+                Stream s = client.GetStream();
+                StreamReader sr = new StreamReader(s);
+                StreamWriter sw = new StreamWriter(s);
+                sw.AutoFlush = true;
+                string ret;
+
+                Console.WriteLine("Name: " + "\n");
+                string name = (a++.ToString());
+                sw.WriteLine(name);
+                ret=sr.ReadLine();
+                Console.WriteLine(ret);
+
+                string message = Console.ReadLine();
+                sw.WriteLine(message);
+                ret = sr.ReadLine();
+                Console.WriteLine(ret);
+                             
+                s.Close();
+            }
+            finally
+            {
+                // code in finally block is guranteed 
+                // to execute irrespective of 
+                // whether any exception occurs or does 
+                // not occur in the try block
+                client.Close();
+            } 
+
+
         }
     }
 }
