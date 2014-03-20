8efa59ae5e355eb3bd7832b7dbcd926f6c8db606
colman79 <colman_no1@hotmail.com>
2014-03-20 23:14:59 +0100

    bla
    bla bla

--- tcpclient/Program.cs
+++ tcpclient/Program.cs
@@ -15,7 +15,7 @@ namespace tcpclient
             TcpClient client = new TcpClient("62.75.224.28", 6789);
             int a = 0;
             try
-            {
+            {//bla bla bla bla bla
                 Stream s = client.GetStream();
                 StreamReader sr = new StreamReader(s);
                 StreamWriter sw = new StreamWriter(s);
